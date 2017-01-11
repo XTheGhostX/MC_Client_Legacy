457131fe1bced8bd4446ab07b289346f4d6e5f93
SFort <mail@sfort.tech>
2017-01-11 21:56:40 +0100

    1st Commit

--- MC_Client/Form1.cs
+++ MC_Client/Form1.cs
@@ -20,6 +20,23 @@ namespace MC_Client
         private void button1_Click(object sender, EventArgs e)
         {
             button1.Text = "Stuff";
+
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=127.0.0.1;uid=root;" +
+                "pwd=12345;database=test;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection(myConnectionString);
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                label1.Text =(ex.Message);
+            }
+
         }
     }
 }
