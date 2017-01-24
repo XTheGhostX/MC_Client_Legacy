7917ba49e940123e68cd5074d01f89b5e364ec3b
sfort <mail@sfort.tech>
2017-01-24 13:49:20 +0100

    Download Zipped Configs

--- MC_Client/Form1.Designer.cs
+++ MC_Client/Form1.Designer.cs
@@ -30,13 +30,18 @@
         {
             this.button_update = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_Versions = new System.Windows.Forms.ComboBox();
+            this.textBox_Path = new System.Windows.Forms.TextBox();
+            this.button_Path = new System.Windows.Forms.Button();
+            this.label_admin = new System.Windows.Forms.Label();
+            this.button_Install = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button_update
             // 
-            this.button_update.Location = new System.Drawing.Point(12, 12);
+            this.button_update.Location = new System.Drawing.Point(151, 9);
             this.button_update.Name = "button_update";
-            this.button_update.Size = new System.Drawing.Size(146, 23);
+            this.button_update.Size = new System.Drawing.Size(121, 23);
             this.button_update.TabIndex = 0;
             this.button_update.Text = "Check For updates";
             this.button_update.UseVisualStyleBackColor = true;
@@ -45,17 +50,75 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Location = new System.Drawing.Point(36, 118);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(48, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Test text";
+            // 
+            // comboBox_Versions
+            // 
+            this.comboBox_Versions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_Versions.Items.AddRange(new object[] {
+            "No Entry"});
+            this.comboBox_Versions.Location = new System.Drawing.Point(151, 38);
+            this.comboBox_Versions.Name = "comboBox_Versions";
+            this.comboBox_Versions.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_Versions.TabIndex = 2;
+            this.comboBox_Versions.SelectedIndexChanged += new System.EventHandler(this.comboBox_Versions_SelectedIndexChanged);
+            // 
+            // textBox_Path
+            // 
+            this.textBox_Path.Enabled = false;
+            this.textBox_Path.Location = new System.Drawing.Point(151, 229);
+            this.textBox_Path.Name = "textBox_Path";
+            this.textBox_Path.Size = new System.Drawing.Size(92, 20);
+            this.textBox_Path.TabIndex = 3;
+            this.textBox_Path.Text = "%appdata%\\.minecraft\\ElementalRealms";
+            this.textBox_Path.TextChanged += new System.EventHandler(this.textBox_Path_TextChanged);
+            // 
+            // button_Path
+            // 
+            this.button_Path.Enabled = false;
+            this.button_Path.Location = new System.Drawing.Point(249, 228);
+            this.button_Path.Name = "button_Path";
+            this.button_Path.Size = new System.Drawing.Size(23, 23);
+            this.button_Path.TabIndex = 4;
+            this.button_Path.Text = "..";
+            this.button_Path.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button_Path.UseVisualStyleBackColor = true;
+            this.button_Path.Click += new System.EventHandler(this.button_Path_Click);
+            // 
+            // label_admin
+            // 
+            this.label_admin.AutoSize = true;
+            this.label_admin.Enabled = false;
+            this.label_admin.Location = new System.Drawing.Point(148, 212);
+            this.label_admin.Name = "label_admin";
+            this.label_admin.Size = new System.Drawing.Size(137, 13);
+            this.label_admin.TabIndex = 5;
+            this.label_admin.Text = "To change dir run as Admin";
+            // 
+            // button_Install
+            // 
+            this.button_Install.Location = new System.Drawing.Point(151, 66);
+            this.button_Install.Name = "button_Install";
+            this.button_Install.Size = new System.Drawing.Size(121, 23);
+            this.button_Install.TabIndex = 6;
+            this.button_Install.Text = "Install";
+            this.button_Install.UseVisualStyleBackColor = true;
+            this.button_Install.Click += new System.EventHandler(this.button_Install_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button_Install);
+            this.Controls.Add(this.label_admin);
+            this.Controls.Add(this.button_Path);
+            this.Controls.Add(this.textBox_Path);
+            this.Controls.Add(this.comboBox_Versions);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button_update);
             this.Name = "Form1";
@@ -69,6 +132,11 @@
 
         private System.Windows.Forms.Button button_update;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_Versions;
+        private System.Windows.Forms.TextBox textBox_Path;
+        private System.Windows.Forms.Button button_Path;
+        private System.Windows.Forms.Label label_admin;
+        private System.Windows.Forms.Button button_Install;
     }
 }
 
--- MC_Client/Form1.cs
+++ MC_Client/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.Security.Principal;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,32 +9,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace MC_Client
 {
     public partial class Form1 : Form
     {
+        //Add method of writing and reading custom install directory
+        public static string AppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        public string Path = AppData+"\\.minecraft\\ElementalRealms";
+        public string ERConnectionString = "server=51.255.41.80;uid=ermlpublicread;" +
+                "pwd=hmDmxuhheilgKXUWTjzC;database=ElementalRealms_ModdedLauncher;";
         public Form1()
         {
             InitializeComponent();
+            bool HasAdminPrivliges;
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            HasAdminPrivliges = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (HasAdminPrivliges)
+            {
+                textBox_Path.Enabled = true;
+                button_Path.Enabled = true;
+                label_admin.Visible = false;
+            }
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             button_update.Enabled = false;
             button_update.Text = "Loading";
+            comboBox_Versions.Text= "";
 
             MySql.Data.MySqlClient.MySqlConnection conn;
-            string myConnectionString;
+            conn = new MySql.Data.MySqlClient.MySqlConnection(ERConnectionString);
 
-            myConnectionString = "server=51.255.41.80;uid=ermlpublicread;" +
-                "pwd=hmDmxuhheilgKXUWTjzC;database=ElementalRealms_ModdedLauncher;";
-            conn = new MySql.Data.MySqlClient.MySqlConnection(myConnectionString);
+            string query = "SELECT * FROM ElementalRealms_ModdedLauncher.Version";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+
             
+            try
+            {
+                conn.OpenAsync();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            comboBox_Versions.Items.Clear();
+            while (dataReader.Read())
+            {
+                comboBox_Versions.Items.Add(dataReader["Version_UID"].ToString());
+                comboBox_Versions.Text=(dataReader["Version_UID"].ToString());
+            }
+
+            conn.CloseAsync();
+            button_update.Enabled = true;
+            button_update.Text = "Check For updates";
+        }
+
+
+
+        private void comboBox_Versions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void textBox_Path_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void button_Path_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_Install_Click(object sender, EventArgs e)
+        {
+
+            button_Install.Enabled = false;
+            comboBox_Versions.Enabled = false;
+            button_update.Enabled = false;
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            conn = new MySql.Data.MySqlClient.MySqlConnection(ERConnectionString);
 
             try
             {
-                
                 conn.OpenAsync();
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
@@ -41,10 +107,42 @@ namespace MC_Client
                 Console.Write(ex.Message);
             }
 
+            string query = "SELECT * FROM ElementalRealms_ModdedLauncher.Version WHERE Version_UID='" + comboBox_Versions.Text+ "'";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+
+            WebClient webClient = new WebClient();
+            string ConfigPath = (Path + "\\TMP\\" + comboBox_Versions.Text + ".zip");
+            DirectoryInfo di = Directory.CreateDirectory(Path + "\\TMP");
+            try {
+                webClient.DownloadFileAsync(new Uri("https://github.com/ElementalRealms/MC_Configs/archive/" + comboBox_Versions.Text + ".zip"), ConfigPath);
+            }
+            catch(Exception ex){
+                Console.WriteLine("The process failed: {0}", ex.ToString());
+            }
             
+            
+            //Unzip config
+
+              //Need A ACTUAL copy if the biome folders
+              //(dataReader["Biome"].ToString());
+
+            //Still need to look into how it is installed nowdays :P
+            //(dataReader["Forge"].ToString());
+
+            //Not sure how it handles with custom directories
+            //(dataReader["Script"].ToString());
+            //Some one else can deal with mods
+            //(dataReader["Mods"].ToString());
+
+
+
             conn.CloseAsync();
+            button_Install.Enabled = true;
+            comboBox_Versions.Enabled = true;
             button_update.Enabled = true;
-            button_update.Text = "Check For updates";
+
         }
     }
 }
