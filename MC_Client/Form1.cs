fed46b74a1ba7385eabe15927ce8a5c758f019f5
SFort <SFort@sfort.tech>
2017-12-30 21:19:52 +0100

    Quick notification change

--- MC_Client/Form1.cs
+++ MC_Client/Form1.cs
@@ -732,6 +732,7 @@ MessageBoxButtons.OK, MessageBoxIcon.Error);
                 {
                     ER_Settings[6] = "LastClientCheck:" + LatestVersion;
                     File.WriteAllLines(Path_Settings, ER_Settings);
+                    if(LastClientVNotification != null)
                     if (MessageBox.Show(MC_client.Result.Name + ":/n"+MC_client.Result.Body + "\n Download?",
                         "Update Notification",
                         MessageBoxButtons.YesNo) == DialogResult.Yes)
